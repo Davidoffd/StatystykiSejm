9dbdaab4b97a1e49a27411e351727f60bbbba4ea
Davidoffd <davidoffd564@gmail.com>
2026-07-05 00:54:32 +0200

    Update update.js

--- scripts/update.js
+++ scripts/update.js
@@ -1,1 +1,22 @@
+const { chromium } = require("playwright");
 
+(async () => {
+
+    const browser = await chromium.launch({
+        headless: true
+    });
+
+    const page = await browser.newPage();
+
+    await page.goto(
+        "https://www.sejm.gov.pl/sejm10.nsf/transmisje_arch.xsp",
+        {
+            waitUntil: "networkidle"
+        }
+    );
+
+    console.log(await page.title());
+
+    await browser.close();
+
+})();
