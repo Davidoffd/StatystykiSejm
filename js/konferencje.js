19b4aabd03e8c566cf73490112320654bc86dd4f
Davidoffd <davidoffd564@gmail.com>
2026-07-05 00:24:01 +0200

    Update konferencje.js

--- js/konferencje.js
+++ js/konferencje.js
@@ -1,1 +1,25 @@
+const dane = [
+    { partia: "PiS", konferencje: 0 },
+    { partia: "KO", konferencje: 0 },
+    { partia: "Konfederacja", konferencje: 0 },
+    { partia: "Polska 2050", konferencje: 0 },
+    { partia: "PSL", konferencje: 0 },
+    { partia: "Lewica", konferencje: 0 },
+    { partia: "Razem", konferencje: 0 }
+];
 
+const tabela = document.getElementById("ranking");
+
+tabela.innerHTML = "";
+
+dane.forEach((partia, index) => {
+
+    tabela.innerHTML += `
+        <tr>
+            <td>${index + 1}</td>
+            <td>${partia.partia}</td>
+            <td>${partia.konferencje}</td>
+        </tr>
+    `;
+
+});
