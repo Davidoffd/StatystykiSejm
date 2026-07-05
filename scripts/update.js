40cd90dec716bbb1021dbed8794c86ea08091f85
Davidoffd <davidoffd564@gmail.com>
2026-07-05 02:52:52 +0200

    Update update.js

--- scripts/update.js
+++ scripts/update.js
@@ -1,9 +1,7 @@
-console.log("NOWA WERSJA SKRYPTU 123456");
 const fs = require("fs");
 
 async function main() {
 
-    // adres API - pierwsze 100 rekordów
     const url = "https://api.sejm.gov.pl/sejm/term10/videos?limit=100";
 
     console.log("Pobieram:", url);
@@ -11,67 +9,41 @@ async function main() {
     const response = await fetch(url);
 
     if (!response.ok) {
-        throw new Error("Błąd API: " + response.status);
+        throw new Error(response.status);
     }
 
     const videos = await response.json();
-    console.log(JSON.stringify(videos[0], null, 2));
-return;
 
-    const ranking = {
-        "PiS": 0,
-        "KO": 0,
-        "Konfederacja": 0,
-        "Polska 2050": 0,
-        "PSL": 0,
-        "Lewica": 0,
-        "Razem": 0
-    };
+    const typy = {};
 
     for (const v of videos) {
 
-        if (v.type !== "konferencja")
-            continue;
+        const type = v.type || "(brak)";
 
-        const txt = (
-            (v.title || "") +
-            " " +
-            (v.description || "")
-        ).toLowerCase();
+        if (!typy[type]) {
+            typy[type] = 0;
+        }
 
-        if (txt.includes("pis"))
-            ranking["PiS"]++;
+        typy[type]++;
 
-        else if (txt.includes("ko"))
-            ranking["KO"]++;
-
-        else if (txt.includes("psl"))
-            ranking["PSL"]++;
-
-        else if (txt.includes("2050"))
-            ranking["Polska 2050"]++;
-
-        else if (txt.includes("lewica"))
-            ranking["Lewica"]++;
-
-        else if (txt.includes("razem"))
-            ranking["Razem"]++;
-
-        else if (txt.includes("konfeder"))
-            ranking["Konfederacja"]++;
     }
 
-    const wynik = Object.entries(ranking).map(([partia, konferencje]) => ({
-        partia,
-        konferencje
-    }));
+    console.log("\n=== TYPY ===\n");
 
-    fs.writeFileSync(
-        "data/konferencje.json",
-        JSON.stringify(wynik, null, 4)
-    );
+    console.table(typy);
 
-    console.log(wynik);
+    console.log("\n=== PRZYKŁADY ===\n");
+
+    for (const t of Object.keys(typy)) {
+
+        const film = videos.find(v => v.type === t);
+
+        console.log("TYPE:", t);
+        console.log("TITLE:", film.title);
+        console.log("---------------------------");
+
+    }
+
 }
 
 main().catch(console.error);
